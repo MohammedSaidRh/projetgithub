fc38b9139329e3939694ba9c8de39dc0dc16ab96
azdad samira <samiraazdad@gmail.com>
2024-01-03 18:17:43 +0100

    Correction de la fonction d'addition

--- src/main/java/ma/ehei/calculatrice/App.java
+++ src/main/java/ma/ehei/calculatrice/App.java
@@ -3,5 +3,6 @@ package ma.ehei.calculatrice;
 public class App {
 	public static void main(String[] args) {
 		System.out.println(Calculatrice.add(5, 5));
+system.out.println(Calculatrice.add(3,3));
 	}
 }
